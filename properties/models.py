f54ddb7c652658527423c4e81cf476f46e444744
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2024-08-15 15:55:39 +0600

    Images now gets deleted from files and Image unique name generation is added

--- properties/models.py
+++ properties/models.py
@@ -1,5 +1,21 @@
+import os
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 from django.utils import timezone
+from django.utils.text import slugify
+
+def generate_image_filename(instance, filename):
+    # Get the file extension
+    ext = filename.split('.')[-1]
+    
+    # Generate the new filename
+    hotel_title = slugify(instance.property_info.title)
+    timestamp = timezone.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"{hotel_title}_{timestamp}.{ext}"
+    
+    # Return the full path
+    return os.path.join('images', filename)
 
 class Location(models.Model):
     COUNTRY = 'country'
@@ -53,13 +69,25 @@ class PropertyInfo(models.Model):
 
 class Image(models.Model):
     property_info = models.ForeignKey(PropertyInfo, related_name='images', on_delete=models.CASCADE)
-    img_path = models.ImageField(upload_to='images/', max_length=255)
+    img_path = models.ImageField(upload_to=generate_image_filename, max_length=255)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.img_path.name
 
+    def delete(self, *args, **kwargs):
+        if self.img_path:
+            if os.path.isfile(self.img_path.path):
+                os.remove(self.img_path.path)
+        super(Image, self).delete(*args, **kwargs)
+
     class Meta:
         db_table = 'image'
         verbose_name_plural = 'Images'
+
+@receiver(pre_delete, sender=Image)
+def delete_image_file(sender, instance, **kwargs):
+    if instance.img_path:
+        if os.path.isfile(instance.img_path.path):
+            os.remove(instance.img_path.path)
