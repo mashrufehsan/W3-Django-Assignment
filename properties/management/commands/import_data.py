041f96b4e9cb3db01e8e580c7eb9d910bc21da48
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2024-08-20 10:57:12 +0600

    Handled keyboard interrupt

--- properties/management/commands/import_data.py
+++ properties/management/commands/import_data.py
@@ -15,119 +15,127 @@ class Command(BaseCommand):
     help = 'Import data after verifying superadmin credentials and print database config'
 
     def handle(self, *args, **options):
-        username = input("Enter superadmin username: ")
-        password = getpass("Enter superadmin password: ")
-        user = authenticate(username=username, password=password)
-        if user is not None and user.is_superuser:
-            self.stdout.write(self.style.SUCCESS('\nAdmin login success!'))
-            db_config = settings.DATABASES['default']
-            self.stdout.write(self.style.WARNING(
-                'Using the database configuration from \'.env\' file.'))
-            import_db_name = os.getenv('SCRAPY_DB_NAME')
-            try:
-                conn = psycopg2.connect(
-                    dbname=import_db_name,
-                    user=db_config['USER'],
-                    password=db_config['PASSWORD'],
-                    host=db_config['HOST'],
-                    port=db_config.get('PORT', '5432')
-                )
-                with conn.cursor() as cur:
-                    cur.execute("""
-                    SELECT table_name
-                    FROM information_schema.tables
-                    WHERE table_schema = 'public'
-                    """)
-                    tables = cur.fetchall()
-                self.stdout.write(self.style.SUCCESS(
-                    f"\nTables in {import_db_name}:"))
-                for i, table in enumerate(tables, 1):
-                    self.stdout.write(self.style.SUCCESS(f"{i}. {table[0]}"))
-                while True:
-                    try:
-                        selection = int(
-                            input("\nEnter the number of the table you want to select (e.g. 1): "))
-                        if 1 <= selection <= len(tables):
-                            selected_table = tables[selection - 1][0]
-                            self.stdout.write(self.style.SUCCESS(
-                                f"You selected: {selected_table}."))
-
-                            with conn.cursor() as cur:
-                                cur.execute(f"SELECT * FROM {selected_table}")
-                                rows = cur.fetchall()
-                                colnames = [desc[0]
-                                            for desc in cur.description]
-                                for row in rows:
-                                    data = dict(zip(colnames, row))
-
-                                    title = data.get('title')
-                                    location_name = data.get('location')
-                                    latitude = data.get('latitude')
-                                    longitude = data.get('longitude')
-                                    img_path = data.get('images')
-
-                                    location, created = Location.objects.get_or_create(
-                                        name=selected_table.capitalize() + ' - ' + location_name,
-                                        type='city',
-                                        defaults={
-                                            'latitude': latitude,
-                                            'longitude': longitude,
-                                        },
-                                    )
-                                    if not created:
-                                        location.latitude = latitude
-                                        location.longitude = longitude
-                                        location.save()
-
-                                    existing_property = PropertyInfo.objects.filter(
-                                        title=title, locations=location).first()
-
-                                    if existing_property:
-                                        self.stdout.write(self.style.WARNING(
-                                            f"Import skipped for {title}. Reason: Already exists with location {location.name}."))
-                                        continue
-
-                                    property_info = PropertyInfo.objects.create(
-                                        title=title,
-                                    )
-                                    property_info.locations.add(location)
-
-                                    import_images_folder_path = os.getenv(
-                                        'IMPORT_IMAGES_FOLDER_PATH')
-                                    full_img_path = os.path.join(
-                                        import_images_folder_path, img_path)
-
-                                    new_img_filename = generate_image_filename(
-                                        None, os.path.basename(full_img_path))
-                                    new_img_path = os.path.join(
-                                        settings.MEDIA_ROOT, new_img_filename)
-
-                                    os.makedirs(os.path.dirname(
-                                        new_img_path), exist_ok=True)
-
-                                    shutil.copy(full_img_path, new_img_path)
-
-                                    Image.objects.create(
-                                        property_info=property_info,
-                                        img_path=new_img_filename,
-                                    )
-
+        try:
+            username = input("Enter superadmin username: ")
+            password = getpass("Enter superadmin password: ")
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_superuser:
+                self.stdout.write(self.style.SUCCESS('\nAdmin login success!'))
+                db_config = settings.DATABASES['default']
+                self.stdout.write(self.style.WARNING(
+                    'Using the database configuration from \'.env\' file.'))
+                import_db_name = os.getenv('SCRAPY_DB_NAME')
+                try:
+                    conn = psycopg2.connect(
+                        dbname=import_db_name,
+                        user=db_config['USER'],
+                        password=db_config['PASSWORD'],
+                        host=db_config['HOST'],
+                        port=db_config.get('PORT', '5432')
+                    )
+                    with conn.cursor() as cur:
+                        cur.execute("""
+                        SELECT table_name
+                        FROM information_schema.tables
+                        WHERE table_schema = 'public'
+                        """)
+                        tables = cur.fetchall()
+                    self.stdout.write(self.style.SUCCESS(
+                        f"\nTables in {import_db_name}:"))
+                    for i, table in enumerate(tables, 1):
+                        self.stdout.write(
+                            self.style.SUCCESS(f"{i}. {table[0]}"))
+                    while True:
+                        try:
+                            selection = int(
+                                input("\nEnter the number of the table you want to select (e.g. 1): "))
+                            if 1 <= selection <= len(tables):
+                                selected_table = tables[selection - 1][0]
                                 self.stdout.write(self.style.SUCCESS(
-                                    f"Data import completed."))
+                                    f"You selected: {selected_table}."))
 
-                            break
-                        else:
+                                with conn.cursor() as cur:
+                                    cur.execute(
+                                        f"SELECT * FROM {selected_table}")
+                                    rows = cur.fetchall()
+                                    colnames = [desc[0]
+                                                for desc in cur.description]
+                                    for row in rows:
+                                        data = dict(zip(colnames, row))
+
+                                        title = data.get('title')
+                                        location_name = data.get('location')
+                                        latitude = data.get('latitude')
+                                        longitude = data.get('longitude')
+                                        img_path = data.get('images')
+
+                                        location, created = Location.objects.get_or_create(
+                                            name=selected_table.capitalize() + ' - ' + location_name,
+                                            type='city',
+                                            defaults={
+                                                'latitude': latitude,
+                                                'longitude': longitude,
+                                            },
+                                        )
+                                        if not created:
+                                            location.latitude = latitude
+                                            location.longitude = longitude
+                                            location.save()
+
+                                        existing_property = PropertyInfo.objects.filter(
+                                            title=title, locations=location).first()
+
+                                        if existing_property:
+                                            self.stdout.write(self.style.WARNING(
+                                                f"Import skipped for {title}. Reason: Already exists with location {location.name}."))
+                                            continue
+
+                                        property_info = PropertyInfo.objects.create(
+                                            title=title,
+                                        )
+                                        property_info.locations.add(location)
+
+                                        import_images_folder_path = os.getenv(
+                                            'IMPORT_IMAGES_FOLDER_PATH')
+                                        full_img_path = os.path.join(
+                                            import_images_folder_path, img_path)
+
+                                        new_img_filename = generate_image_filename(
+                                            None, os.path.basename(full_img_path))
+                                        new_img_path = os.path.join(
+                                            settings.MEDIA_ROOT, new_img_filename)
+
+                                        os.makedirs(os.path.dirname(
+                                            new_img_path), exist_ok=True)
+
+                                        shutil.copy(
+                                            full_img_path, new_img_path)
+
+                                        Image.objects.create(
+                                            property_info=property_info,
+                                            img_path=new_img_filename,
+                                        )
+
+                                    self.stdout.write(self.style.SUCCESS(
+                                        f"Data import completed."))
+
+                                break
+                            else:
+                                self.stdout.write(self.style.ERROR(
+                                    "Invalid selection. Please try again."))
+                        except ValueError:
                             self.stdout.write(self.style.ERROR(
-                                "Invalid selection. Please try again."))
-                    except ValueError:
-                        self.stdout.write(self.style.ERROR(
-                            "Invalid input. Please enter a number."))
-                conn.close()
+                                "Invalid input. Please enter a number."))
+                    conn.close()
 
-            except psycopg2.Error as e:
+                except psycopg2.Error as e:
+                    self.stdout.write(self.style.ERROR(
+                        f"Unable to connect to the database: {e}"))
+
+            else:
                 self.stdout.write(self.style.ERROR(
-                    f"Unable to connect to the database: {e}"))
+                    'Invalid credentials or user is not a superadmin'))
 
-        else:
+        except KeyboardInterrupt:
             self.stdout.write(self.style.ERROR(
-                'Invalid credentials or user is not a superadmin'))
+                "\nProcess interrupted. Exiting..."))
